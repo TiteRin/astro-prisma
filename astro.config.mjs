fd8c7b02fdb1cfa380a0c15c760bf6fc18076957
TiteRin <marine.lancien@protonmail.com>
2025-04-22 09:50:36 +0200

    feat: update to Astro 5.7 and configure Netlify adapter for static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
 import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: netlify(),
   integrations: [
     mdx(), 
     icon(), 
